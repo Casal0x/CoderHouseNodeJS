57ae4db12ee150b6c0bea1fdcfe8d657d58dbf6d
Sebastian Casal <sebastian.casal@gmail.com>
2021-11-23 19:56:20 -0300

    testing

--- src/tests/productos.test.ts
+++ src/tests/productos.test.ts
@@ -1,132 +1,23 @@
-// import axios from 'axios';
-// import { expect } from 'chai';
+import axios from 'axios';
+import { expect } from 'chai';
 
-// describe('Test Productos', () => {
-//   test('Deberia devolver una logitud mayor a 0 y status 200', async () => {
-//     const { data, status } = await axios.get(
-//       'http://localhost:8080/api/productos/listar'
-//     );
+describe('Test Productos', () => {
+  test('Deberia devolver una logitud mayor a 0 y status 200', async () => {
+    const { data, status } = await axios.get(
+      'http://localhost:8080/api/productos/listar'
+    );
 
-//     expect(data.length).to.not.be.equal(0);
-//     expect(status).to.equal(200);
-//   });
-
-//   test('El producto seleccionado debe tener el codigo A4GSD3442', async () => {
-//     const idProducto = '6192d53804ee46dd46af1be9';
-//     const codigoEsperado = 'A4GSD3442';
-//     const { data } = await axios.get(
-//       `http://localhost:8080/api/productos/listar/${idProducto}`
-//     );
-
-//     expect(data.product.code).to.equal(codigoEsperado);
-//   });
-// });
-import Server from '../services/server';
-import supertest, { SuperAgentTest } from 'supertest';
-import MongoMemoryServer from 'mongodb-memory-server-core';
-import mongoose from 'mongoose';
-import { addProductsMockDb, addUserAndLogin } from 'utils/testUtils';
-// import { IItem } from 'common/interfaces/products';
-
-describe('Productos api tests', () => {
-  let request: SuperAgentTest;
-
-  beforeAll(async () => {
-    request = supertest.agent(Server);
-    await addProductsMockDb();
-    await addUserAndLogin(request);
+    expect(data.length).to.not.be.equal(0);
+    expect(status).to.equal(200);
   });
 
-  afterAll(async () => {
-    const instance: MongoMemoryServer = global.__MONGOINSTANCE__;
-    // Server.close();
-    await instance.stop();
-    await mongoose.disconnect();
+  test('El producto seleccionado debe tener el codigo A4GSD3442', async () => {
+    const idProducto = '6192d53804ee46dd46af1be9';
+    const codigoEsperado = 'A4GSD3442';
+    const { data } = await axios.get(
+      `http://localhost:8080/api/productos/listar/${idProducto}`
+    );
+
+    expect(data.product.code).to.equal(codigoEsperado);
   });
-
-  it('GET: should return a list of products, 200 status code', async () => {
-    // const response = await request.get('/api/productos/listar');
-    // const productsArray = response.body.data;
-
-    // expect(productsArray.length).not.toBe(0);
-    // expect(response.status).toBe(200);
-    expect(true).toBe(true);
-  });
-
-  //   it('GET: should return a product by its id, 200 status code', async () => {
-  //     const response = await request.get('/api/productos');
-  //     const expectedProductId = response.body.data[0].id;
-  //     const productResponse = await request.get(
-  //       `/api/productos/${expectedProductId}`,
-  //     );
-
-  //     const productId = productResponse.body.data.id;
-
-  //     expect(productResponse.statusCode).toBe(200);
-  //     expect(expectedProductId).toEqual(productId);
-  //   });
-
-  //   it('POST: should add a product and return it, 200 status code', async () => {
-  //     const mockProduct = {
-  //       nombre: 'Test product',
-  //       descripcion:
-  //         'Lorem ipsum dolor sit amet, nam fierent perfecto ea, pro in albucius oportere accommodare.',
-  //       codigo: 'ECOM-1234-1234',
-  //       precio: 123.8,
-  //       foto: 'https://picsum.photos/300?random=2',
-  //       stock: 44,
-  //     };
-  //     const response = await request.post('/api/productos').send(mockProduct);
-
-  //     const products = (await request.get('/api/productos')).body.data;
-
-  //     const productAddedToDb = products.find(
-  //       (item: IItem) => item.nombre === 'Test product',
-  //     );
-
-  //     expect(response.statusCode).toBe(200);
-  //     expect(response.body.data.nombre).toEqual('Test product');
-  //     expect(productAddedToDb.nombre).toEqual('Test product');
-  //   });
-
-  //   it('PUT: should edit a product and return it, 200 status code', async () => {
-  //     const productsResponse = await request.get('/api/productos');
-  //     const productToEdit = productsResponse.body.data[0];
-
-  //     const newProductData = {
-  //       ...productToEdit,
-  //       nombre: 'Put Test',
-  //     };
-
-  //     const putResponse = await request
-  //       .put(`/api/productos/${productToEdit.id}`)
-  //       .send(newProductData);
-  //     const editedProduct = putResponse.body.data;
-
-  //     const productEditedInDb = (
-  //       await request.get(`/api/productos/${editedProduct.id}`)
-  //     ).body.data;
-
-  //     expect(putResponse.statusCode).toBe(200);
-  //     expect(editedProduct.nombre).toEqual('Put Test');
-  //     expect(productEditedInDb.nombre).toEqual('Put Test');
-  //   });
-
-  //   it('DELETE: should delete a product by its id, 200 status code', async () => {
-  //     const productsResponse = await request.get('/api/productos');
-  //     const productToDelete = productsResponse.body.data[0];
-
-  //     const deleteResponse = await request.delete(
-  //       `/api/productos/${productToDelete.id}`,
-  //     );
-
-  //     const productsAfterDelete = (await request.get('/api/productos')).body.data;
-  //     const productDeleted = productsAfterDelete.find(
-  //       (item: IItem) => item.id === productToDelete.id,
-  //     );
-
-  //     expect(productsResponse.statusCode).toBe(200);
-  //     expect(deleteResponse.body.data).toBe('Producto eliminado');
-  //     expect(productDeleted).toBeUndefined();
-  //   });
 });
