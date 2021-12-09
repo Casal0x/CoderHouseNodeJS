72a86c6f13a6333082f6dbbab55717c449bc8e84
Sebastian Casal <sebastian.casal@gmail.com>
2021-12-09 20:27:03 -0300

    Clase_44

--- src/interfaces/types.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { IUser } from './user.interface';
-
-export interface MRequest extends Request {
-  user: IUser;
-}
--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-import MongoMemoryServer from 'mongodb-memory-server-core';
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      __MONGOINSTANCE__: MongoMemoryServer;
-    }
-  }
-}
